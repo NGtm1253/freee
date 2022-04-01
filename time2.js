3aa7ce3fa002a9608bb698b869d97c2d980e8500
NGtm1253 <101865089+NGtm1253@users.noreply.github.com>
2022-04-01 22:51:56 +0900

    Add files via upload

--- time2.js
+++ time2.js
@@ -9,5 +9,5 @@
         var week = data.getDay();
         var day = data.getDate();
         var week_ja= new Array("日","月","火","水","木","金","土");
-        time_el.innerHTML = year+"年"+month+"月"+day+"日"+week_ja[week]+"曜日"+hour+"時"+min+"分"+sec+"秒";
+        time_el.innerHTML = year+"年"+month+"月"+day+"日"+week_ja[week]+"曜日 "+hour+"時"+min+"分"+sec+"秒";
     },1000);
